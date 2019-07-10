e485cf9060b0927170a33987f531515e44b17239
Luca Baffa <lb803@mailbox.org>
2019-07-10 23:11:45 +0100

    added support for argparse

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 import json
 import os
+import argparse
 
-# DEBUG open the example file
-input_metadata = os.path.join(os.path.dirname(__file__),
-                              'example-monograph.json')
+parser = argparse.ArgumentParser(description='obp-doi-deposit')
+parser.add_argument('metadata',
+                    help = 'JSON file where input metadata is stored')
+
+args = parser.parse_args()
+
+# Store input file path and verify that it extsts
+input_metadata = os.path.abspath(args.metadata)
+if not os.path.exists(input_metadata):
+    raise ValueError("The file {} doesn't exist".format(input_metadata))
 
 with open(input_metadata, 'r') as f:
     metadata = json.load(f)
